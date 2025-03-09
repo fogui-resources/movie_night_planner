7ce63a6b7a40a3eb9679e5bfd17daf4154236fd8
REZA <reza.karami.arbeiten@gmail.com>
2025-03-09 23:57:22 +0100

    Added a frontend section for the project

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routers import movies, tv_shows
 
 app = FastAPI(title="Movie & TV Show Recommender API")
+
+# Serve the static frontend
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Include routers for different functionalities
 app.include_router(movies.router)
